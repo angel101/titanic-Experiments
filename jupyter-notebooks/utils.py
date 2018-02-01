877bfc60d573b15ddcaf5ab18de4139a700c8be9
Carlos Aguirre <carlosaguirre@keos.co>
2018-02-01 09:38:09 -0500

    commit tdnn titanic

--- jupyter-notebooks/utils.py
+++ jupyter-notebooks/utils.py
@@ -2,13 +2,15 @@ from tflearn.datasets import titanic
 from tflearn.data_utils import load_csv
 
 import pandas
+import numpy as np
 
 class Utils():
 	
 	def downloadDataset():
 		titanic.download_dataset()
-		data, labels = load_csv('titanic_dataset.csv', target_column=0,
-                       categorical_labels=True, n_classes=2)
+		data, labels = load_csv('train.csv', target_column=1,
+                       categorical_labels=True, n_classes=2,
+                       columns_to_ignore=[0,8,10])
 
 		return data,labels
 	
@@ -17,16 +19,21 @@ class Utils():
 		data,labels = Utils.downloadDataset()
 
 		dict_novelties=[]
-		
+		dict_salidas=[]
 		#feed titles dictionary 
 		for item in data:
 			if not item[1].split(',')[1].strip().split(' ')[0] in dict_novelties:
 				dict_novelties.append(item[1].split(',')[1].strip().split(' ')[0])
+			if not item[7] in dict_salidas:
+				dict_salidas.append(item[7])
 
 
+		print(dict_novelties)
+		print(dict_salidas)
 		dataset = []
 	    #get dummy variables for the name titles ('mr,ms,doctor, etc')
 		dummiesDataFrame = pandas.get_dummies(dict_novelties)
+		dummiesSalida = pandas.get_dummies(dict_salidas)
 	    #parce dataset to numeric values 
 		for item in data:
 			tmp = []
@@ -35,12 +42,13 @@ class Utils():
 				tmp.append(dummiesDataFrame.index[dummiesDataFrame[item[1].split(',')[1].strip().split(' ')[0]]==True].tolist()[0])
 
 			tmp.append(1 if item[2]=='female' else 0)
-			tmp.append(float(item[3]))
+			tmp.append(float(item[3] if item[3]!= '' else 0))
 			tmp.append(int(item[4]))
 			tmp.append(int(item[5]))
-			tmp.append(float(item[7]))
+			tmp.append(item[6])
+			tmp.append(dummiesSalida.index[dummiesSalida[item[7]]==True].tolist()[0])
 			dataset.append(tmp)
-		dataset = pandas.DataFrame(dataset)
+		dataset = np.array(dataset)
 
 
 		Y=[]
